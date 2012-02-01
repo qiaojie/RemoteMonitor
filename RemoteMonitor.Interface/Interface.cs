5d400e3eb1515b219fec0824eae3e5fef9405ffb
qiaojie <qiaojie@gmail.com>
2012-02-01 17:24:42 +0800

    add interface project
    Signed-off-by: qiaojie <qiaojie@gmail.com>

---
+++ RemoteMonitor.Interface/Interface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using EGL;
+using EGL.Communication;
+
+namespace RemoteMonitor
+{
+	[Serializable()]
+	public class ClientData
+	{
+		public string Name;
+		public int CPU;
+		public long Memory;
+	}
+
+	[Protocol(ID = 0x489ABE4F, Version = 1)]
+	public interface IRemoteMonitor
+	{
+		[DispId(1)]
+		void UpdateData(ClientData data);
+	}
+}
